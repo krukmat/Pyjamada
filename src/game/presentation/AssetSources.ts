4fecc9e061c888d73790e35bf46dac11e06047d7
Matias Kruk <kruk.matias@gmail.com>
2026-09-14 21:20:04 +0200

    feat: register haunted Wally atlas asset

--- src/game/presentation/AssetSources.ts
+++ src/game/presentation/AssetSources.ts
@@ -1,5 +1,6 @@
 declare const require: (path: string) => number;
 
 export const WALLY_ATLAS_SOURCE = require('../../../assets/game/wally/wally.png');
+export const HAUNTED_WALLY_ATLAS_SOURCE = require('../../../assets/game/wally/haunted-wally.png');
 export const BEDROOM_OBJECTS_ATLAS_SOURCE = require('../../../assets/game/objects/bedroom-objects.png');
 export const DOMESTIC_FX_ATLAS_SOURCE = require('../../../assets/game/fx/domestic-fx.png');
