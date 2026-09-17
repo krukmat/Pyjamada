bba3b46c97897eead62b0d75b645d0c6c6475147
Matias Kruk <kruk.matias@gmail.com>
2026-09-17 15:24:06 +0200

    test: expect 33 Android screenshots for W4

--- scripts/android-screenshots.test.mjs
+++ scripts/android-screenshots.test.mjs
@@ -23,6 +23,8 @@ const EXPECTED = [
   '22_living_room_source_cue.png', '23_kitchen_arrival.png', '24_kitchen_overload.png',
   '25_kitchen_power_rerouted.png', '26_bathroom_arrival.png', '27_bathroom_mirror_mismatch.png',
   '28_bathroom_reflected_route.png', '29_bathroom_route_revealed.png',
+  '30_attic_arrival.png', '31_attic_evidence.png', '32_attic_recording.png',
+  '33_basement_route_revealed.png',
 ];
 
 function writeExecutable(filePath, script) {
